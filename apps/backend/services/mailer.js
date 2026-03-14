b174309fabce8002bde7468d38828dba71824759
Benkó Barnabás <h450287@stud.u-szeged.hu>
2026-03-14 22:55:22 +0000

    Feature/hosting and verification

--- apps/backend/services/mailer.js
+++ apps/backend/services/mailer.js
@@ -2,12 +2,13 @@ import { createTransport } from "nodemailer";
 
 const transporter = createTransport({
   host: process.env.SMTP_HOST,
-  port: process.env.SMTP_PORT,
-  secure: false,
+  port: parseInt(process.env.SMTP_PORT),
+  secure: false, 
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS
-  }
+  },
+  requireTLS: true
 });
 
 export async function sendMail({ to, subject, html }) {
