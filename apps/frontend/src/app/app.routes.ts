350a1a561a1498c876c1104128c4b8b30e4f7230
Császár Dominik <csaszardominik1@gmail.com>
2026-03-29 14:58:17 +0200

    Creating LandingPage and EventPage

--- apps/frontend/src/app/app.routes.ts
+++ apps/frontend/src/app/app.routes.ts
@@ -2,10 +2,16 @@ import { Routes } from '@angular/router';
 import { Login } from './auth/login/login';
 import { Registration } from './auth/registration/registration';
 import { ForgotPassword } from './auth/forgot-password/forgot-password';
+import {Landing} from './auth/landing/landing';
+import {EventComponent } from './auth/event/event';
+
+
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: Login },
   { path: 'registration', component: Registration },
-  { path: 'forgot-password', component: ForgotPassword } // új route
+  { path: 'forgot-password', component: ForgotPassword }, // új route
+  { path: 'landing', component: Landing },
+  { path: 'events', component: EventComponent }
 ];
