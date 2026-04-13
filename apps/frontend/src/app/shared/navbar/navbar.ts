dd2285a0514dd3d541ce17bd640d5a61edfe4a20
Tüth Bálint <h165388@stud.u-szeged.hu>
2026-04-13 04:38:58 +0200

    #83 Refactor navbar component; replaced sidebar with a drawer layout.

--- apps/frontend/src/app/shared/navbar/navbar.ts
+++ apps/frontend/src/app/shared/navbar/navbar.ts
@@ -1,14 +1,17 @@
 import { Component, inject } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { DrawerModule } from 'primeng/drawer';
 import { ButtonModule } from 'primeng/button';
 import { Auth } from '../../auth/services/auth';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink, RouterLinkActive, ButtonModule],
+  imports: [RouterLink, RouterLinkActive, DrawerModule, ButtonModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
 export class Navbar {
   authService = inject(Auth);
+
+  visible: boolean = false;
 }
