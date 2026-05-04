c0cb761c5bab54d3dff3ffe399c3af4c2838f04a
Tassy Zsolt <h165103@stud.u-szeged.hu>
2026-05-04 05:32:13 +0000

    Feature/news frontend ui

---
+++ apps/frontend/src/app/news/services/news.service.ts
@@ -0,0 +1,38 @@
+import { inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
+import { Observable } from 'rxjs';
+
+export interface News {
+  id: number;
+  title: string;
+  content: string;
+  createdAt: string;
+  expiresAt?: string | null;
+  createdBy?: number;
+  imageUrl?: string | null;
+  creator?: {
+    id: number;
+    username: string;
+    displayName?: string | null;
+    profilePicture?: string | null;
+  };
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NewsService {
+  private http = inject(HttpClient);
+  private apiUrl = `${environment.apiUrl}/news`;
+
+  // latest 3 news for landing page
+  getLatestNews(): Observable<News[]> {
+    return this.http.get<News[]>(`${this.apiUrl}/latest`);
+  }
+  // all news for news page
+  getAllNews(): Observable<News[]> {
+    return this.http.get<News[]>(this.apiUrl);
+  }
+
+}
